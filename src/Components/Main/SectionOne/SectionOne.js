38accb7e428af8d53d6ad1199f5bd49efb90b10e
nimatorabi <46302001+nimatorabi@users.noreply.github.com>
2020-03-24 14:51:28 +0430

    add header title and button to main section part section One

--- src/Components/Main/SectionOne/SectionOne.js
+++ src/Components/Main/SectionOne/SectionOne.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { Row, Container, Col, Button } from 'react-bootstrap';
+import "./SectionOne.css";
 const SectionOne = () => {
     return (
         <Container fluid>
             <Row className="mt-4">
-                <Col md={8}>
-                    <div className="">
+                <Col md={{ span: 8, offset: 1 }}>
+                    <div className="d-flex justify-content-between">
                         <h5>
+                            <i className="fa fa-film ml-2"></i>
                             آخرین فیلم های روز ایران</h5>
-                        <Button className="d-inline">
+                        <Button className="d-inline ButtonHome">
                             فیلم های بیشتر
                         <i className=" mr-2 fa fa-chevron-left"></i>
                         </Button>
                     </div>
+                    <p className="d-flex justify-content-start mr-4 text-muted text-black-50">فیلم های روی پرده سینما در این هفته ...</p>
                 </Col>
             </Row>
         </Container>
